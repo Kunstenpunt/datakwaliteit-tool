a9810e46865eb9ec753b9295b8152adbadfaaa99
Christophe Van den Abbeele <christophe.vda@protonmail.com>
2025-12-05 19:21:55 +0100

    Implement UI for input/output limits and paging.

--- ui/constrainttab.py
+++ ui/constrainttab.py
@@ -15,16 +15,16 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QHBoxLayout,
-    QHeaderView, QLabel, QPushButton, QSizePolicy,
-    QSpacerItem, QSplitter, QTableView, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QComboBox,
+    QGridLayout, QHBoxLayout, QHeaderView, QLabel,
+    QPushButton, QSizePolicy, QSpacerItem, QSpinBox,
+    QSplitter, QTableView, QVBoxLayout, QWidget)
 
 class Ui_ConstraintTab(object):
     def setupUi(self, ConstraintTab):
         if not ConstraintTab.objectName():
             ConstraintTab.setObjectName(u"ConstraintTab")
-        ConstraintTab.resize(980, 657)
+        ConstraintTab.resize(1323, 657)
         self.verticalLayout = QVBoxLayout(ConstraintTab)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.splitter = QSplitter(ConstraintTab)
@@ -95,7 +95,7 @@ class Ui_ConstraintTab(object):
         self.verticalLayoutRight.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayoutRight = QHBoxLayout()
         self.horizontalLayoutRight.setObjectName(u"horizontalLayoutRight")
-        self.horizontalSpacerRight1 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.horizontalSpacerRight1 = QSpacerItem(40, 20, QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
 
         self.horizontalLayoutRight.addItem(self.horizontalSpacerRight1)
 
@@ -107,14 +107,68 @@ class Ui_ConstraintTab(object):
 
         self.horizontalLayoutRight.addWidget(self.labelRight)
 
-        self.horizontalSpacerRight2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.horizontalSpacerRight2 = QSpacerItem(40, 20, QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Minimum)
 
         self.horizontalLayoutRight.addItem(self.horizontalSpacerRight2)
+
+        self.gridLayoutRight = QGridLayout()
+        self.gridLayoutRight.setObjectName(u"gridLayoutRight")
+        self.limitLabel = QLabel(self.verticalLayoutWidgetRight)
+        self.limitLabel.setObjectName(u"limitLabel")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.limitLabel.sizePolicy().hasHeightForWidth())
+        self.limitLabel.setSizePolicy(sizePolicy)
+        self.limitLabel.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+
+        self.gridLayoutRight.addWidget(self.limitLabel, 2, 0, 1, 1)
+
+        self.pageLabel = QLabel(self.verticalLayoutWidgetRight)
+        self.pageLabel.setObjectName(u"pageLabel")
+        sizePolicy.setHeightForWidth(self.pageLabel.sizePolicy().hasHeightForWidth())
+        self.pageLabel.setSizePolicy(sizePolicy)
+        self.pageLabel.setAlignment(Qt.AlignmentFlag.AlignRight|Qt.AlignmentFlag.AlignTrailing|Qt.AlignmentFlag.AlignVCenter)
+
+        self.gridLayoutRight.addWidget(self.pageLabel, 3, 0, 1, 1)
+
+        self.limitSpinBox = QSpinBox(self.verticalLayoutWidgetRight)
+        self.limitSpinBox.setObjectName(u"limitSpinBox")
+        self.limitSpinBox.setMaximum(100000)
+        self.limitSpinBox.setSingleStep(10000)
+        self.limitSpinBox.setValue(100000)
+
+        self.gridLayoutRight.addWidget(self.limitSpinBox, 2, 1, 1, 1)
 
         self.validateButton = QPushButton(self.verticalLayoutWidgetRight)
         self.validateButton.setObjectName(u"validateButton")
 
-        self.horizontalLayoutRight.addWidget(self.validateButton)
+        self.gridLayoutRight.addWidget(self.validateButton, 3, 2, 1, 1)
+
+        self.comboBox = QComboBox(self.verticalLayoutWidgetRight)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+
+        self.gridLayoutRight.addWidget(self.comboBox, 2, 2, 1, 1)
+
+        self.pageSpinBox = QSpinBox(self.verticalLayoutWidgetRight)
+        self.pageSpinBox.setObjectName(u"pageSpinBox")
+        self.pageSpinBox.setAutoFillBackground(False)
+        self.pageSpinBox.setFrame(True)
+        self.pageSpinBox.setMinimum(1)
+        self.pageSpinBox.setValue(1)
+
+        self.gridLayoutRight.addWidget(self.pageSpinBox, 3, 1, 1, 1)
+
+        self.inputRowsLabel = QLabel(self.verticalLayoutWidgetRight)
+        self.inputRowsLabel.setObjectName(u"inputRowsLabel")
+        self.inputRowsLabel.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
+
+        self.gridLayoutRight.addWidget(self.inputRowsLabel, 1, 2, 1, 1)
+
+
+        self.horizontalLayoutRight.addLayout(self.gridLayoutRight)
 
 
         self.verticalLayoutRight.addLayout(self.horizontalLayoutRight)
@@ -133,6 +187,7 @@ class Ui_ConstraintTab(object):
 
         self.violationsLabel = QLabel(self.verticalLayoutWidgetRight)
         self.violationsLabel.setObjectName(u"violationsLabel")
+        self.violationsLabel.setTextInteractionFlags(Qt.TextInteractionFlag.LinksAccessibleByMouse|Qt.TextInteractionFlag.TextSelectableByMouse)
 
         self.horizontalLayoutRightBottom.addWidget(self.violationsLabel)
 
@@ -172,7 +227,13 @@ class Ui_ConstraintTab(object):
         self.exportAllUrlCheckBox.setText(QCoreApplication.translate("ConstraintTab", u"Full URLs", None))
         self.labelRight.setText(QCoreApplication.translate("ConstraintTab", u"Selected Constrained Property\n"
 "will be displayed here", None))
+        self.limitLabel.setText(QCoreApplication.translate("ConstraintTab", u"Limit", None))
+        self.pageLabel.setText(QCoreApplication.translate("ConstraintTab", u"Page", None))
         self.validateButton.setText(QCoreApplication.translate("ConstraintTab", u"Validate", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("ConstraintTab", u"Limit Output Rows", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("ConstraintTab", u"Limit Input Rows", None))
+
+        self.inputRowsLabel.setText(QCoreApplication.translate("ConstraintTab", u"Rows to check: ?", None))
         self.violationsLabel.setText(QCoreApplication.translate("ConstraintTab", u"Not Validated", None))
         self.exportButton.setText(QCoreApplication.translate("ConstraintTab", u"Export", None))
         self.exportUrlCheckBox.setText(QCoreApplication.translate("ConstraintTab", u"Full URLs", None))
