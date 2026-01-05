62c16757ee3d1b2712169e6295eb5b8eb27424bd
Christophe Van den Abbeele <christophe.vda@protonmail.com>
2026-01-05 12:42:13 +0100

    Add util function to visually align text columns.

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -41,3 +41,36 @@ def urlFromId(possibleId, baseUrl):
         return f"{baseUrl}/entity/statement/{possibleId}"
     else:
         return None
+
+
+def alignTabColumns(text, tabWidth, nCols):
+    """Modify tab separated text to create aligned columns
+
+    This function will take an input text containing data in columns that are separated by single tabs, and add tabs
+    where needed to make the columns visually align.
+
+    :param text: The input data, containing one or more lines of single tab separated columns
+    :param tabWidth: The width of the full tab character in number of characters
+    :param nCols: The number of columns in the input text
+    :return: The modified text with extra tabs for visually aligned columns
+    """
+    maxWidths = [0] * (nCols - 1)
+    lines = text.split("\n")
+    for line in lines:
+        for i, w in enumerate(line.split("\t")):
+            if i == nCols - 1:
+                break
+            maxWidths[i] = max(len(w), maxWidths[i])
+    result = ""
+    maxWidths = [((w // tabWidth) + 1) * tabWidth for w in maxWidths]
+    print(maxWidths)
+    for line in lines:
+        for i, w in enumerate(line.split("\t")):
+            result += w
+            if i == nCols - 1:
+                break
+            spacesLeft = maxWidths[i] - len(w)
+            nTabs = (max(spacesLeft - 1, 0) // tabWidth) + 1
+            result += "\t" * nTabs
+        result += "\n"
+    return result
