4c24f87072b73c908ab7f0a6fb023035b1704dde
Christophe Van den Abbeele <christophe.vda@protonmail.com>
2026-02-22 19:21:33 +0100

    Make sure baseUrl doesn't end in '/' in urlFromId.

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -38,6 +38,8 @@ def urlFromId(possibleId: str, baseUrl: str) -> Optional[str]:
     entityRegex = re.compile(r"^Q\d+$")
     statementRegex = re.compile(r"^[PQ]\d+-[A-Za-z0-9\-]+$")
 
+    baseUrl = baseUrl.rstrip("/")
+
     if propertyRegex.match(possibleId) or entityRegex.match(possibleId):
         return f"{baseUrl}/entity/{possibleId}"
     elif statementRegex.match(possibleId):
