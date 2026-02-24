4de80285a2fc9e51b419054db1f8451115d25363
Christophe Van den Abbeele <christophe.vda@protonmail.com>
2026-02-24 12:21:45 +0100

    Clean up batcheditor.py .

--- src/backend/batcheditor.py
+++ src/backend/batcheditor.py
@@ -10,7 +10,6 @@ from .wikibasehelper import WikibaseConfig, WikibaseQueryRunner
 
 class BatchEditor(QObject):
 
-    inputDataObtained = Signal()
     statementGenerationDone = Signal()
 
     def __init__(
@@ -25,8 +24,6 @@ class BatchEditor(QObject):
         self.query = ""
         self.recipe = ""
         self.generatedStatements = ""
-
-        self.inputDataObtained.connect(self._generateEditStatements)
 
     def startPipeline(self, query: str, recipe: str) -> None:
         self.query = query
@@ -53,28 +50,32 @@ class BatchEditor(QObject):
 
     def _executeQueryResult(self) -> None:
         self.inputData = self.wikibaseQueryRunner.queryResult
-        self.inputDataObtained.emit()
+        self._generateEditStatements()
 
     def _generateEditStatements(self) -> None:
-        result = []
         if not self.inputData:
             return
 
         self.inputData = stripUrlPartFromTable(
             self.wikibaseConfig.getBaseUrl(), self.inputData
         )
-        recipeFormatStr = self._prepareRecipeFormatStr(self.inputData[0])
 
-        for row in self.inputData[1:]:
-            result += [recipeFormatStr.format(*row)]
+        recipeFormatStr = self._generateRecipeFormatStr(self.inputData[0])
+        self._applyRecipeFormatStr(recipeFormatStr)
 
-        self.generatedStatements = "\n".join(result)
         self.statementGenerationDone.emit()
 
-    def _prepareRecipeFormatStr(self, header: Sequence[str]) -> str:
+    def _generateRecipeFormatStr(self, header: Sequence[str]) -> str:
         recipeFormatStr = self.recipe
         # Sort from long to short to make sure we do not replace substrings of variable names.
         headerSorted = sorted(enumerate(header), key=lambda i: len(i[1]), reverse=True)
         for i, varName in headerSorted:
             recipeFormatStr = recipeFormatStr.replace("?" + varName, f"{{{i}}}")
         return recipeFormatStr
+
+    def _applyRecipeFormatStr(self, recipeFormatStr):
+        result = []
+        for row in self.inputData[1:]:
+            result += [recipeFormatStr.format(*row)]
+
+        self.generatedStatements = "\n".join(result)
